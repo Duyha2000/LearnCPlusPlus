4359ac2426b9d128ad40f64ad3f02ed3ef511ce0
Duyha2000 <duytd19405c@st.uel.edu.vn>
2025-01-06 15:29:41 +0700

    push code

--- main.cpp
+++ main.cpp
@@ -7,26 +7,44 @@ using namespace std;
 // Bài toán đếm số: count = 0 -> Khi thỏa mãn điều kiện(if else) count++
 // Bài tóan vị trí: position = -1 -> Khi thỏa mãn điều kiện (if else) pos = i
 // i : dùng bài toán liên quan vị trí (100%)
-// Trong if else so sánh dùng 2 dấu = " == "
+// Trong if else so sánh dùngareaCircle(double r) 2 dấu = " == "
 // Không in ra sum và count trong vòng lặp
 
-int main() {
-    string word;
-    getline(cin, word);
-    int countNumber = 0;
-    int countChar = 0;
-    for (int i = 0; i < word.size(); i++) {
-        if (word[i] >= '0' && word[i] <= '9') {
-            countNumber++;
+// https://docs.google.com/document/d/130YMvQO2tqBXioqX3mvP7hk9l58QUsWZmrhXUXrG_h4/edit?tab=t.0#heading=h.b945vx857czl
+void perNumber(int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (n % i == 0) {
+            sum = sum + i;
         }
-        if ((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z'))
-            countChar++;
     }
-    cout << "So ky tu dac biet la: " << word.size() - countChar - countNumber << endl;
-    cout << countNumber << endl;
-    cout << countChar << endl;
-    // "Hello123@"
-    // 9 ky tu: 5 chu - 3 số - 1 [đặc biệt ]
+    if (sum == n) {
+        cout << n << " la so hoan hao";
+    } else {
+        cout << n << " ko la so hoan hao";
+    }
+}
+
+
+int main() {
+    // Exercise 10: Check Perfect Number
+    // Input: 28: các ước là 1 2 4 7 14
+    // Tổng các ưoc sô = số input  (PerfectNumber)
+    // Output: PerfectNumber
+
+    // Input: 10: các ước là 1 2 5
+    // Tổng các ước số = 8 != 10 ( k phải perfect number)
+    int n;
+    cin >> n;
+    perNumber(n);
     return 0;
 }
+
+
+
+
+
+
+
+
 
